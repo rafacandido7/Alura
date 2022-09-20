f6cbe4bf12b5d9ad399b0780b9c2e12b250d2516
Rafael Candido <rafacandido0708@gmail.com>
2022-09-20 11:52:31 -0300

    02.Trabalhando com Express

--- NodeJs/bookstore/server.js
+++ NodeJs/bookstore/server.js
@@ -1,17 +1,7 @@
-const http = require("http")
-const port = 3000
+import app from './src/app.js'
+const port = process.env.PORT || 3000
 
-const rotas = {
-    '/': 'Curso d Node',
-    '/livros': 'Entrei na pag de livros',
-    '/autores': 'Listagem de Autores'
-}
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'})
-    res.end(rotas[req.url])
-})
-
-server.listen(port, () => {
+app.listen(port, () => {
     console.log(`Servidor escutando em http://localhost:${port}`)
 })
