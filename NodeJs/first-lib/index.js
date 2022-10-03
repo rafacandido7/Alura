ad63f50ac0e9fa12371b370ce1d00977052b0ec5
Rafael Candido <rafacandido0708@gmail.com>
2022-10-03 19:45:39 -0300

    01.Criando um Projeto em Node.js

---
+++ NodeJs/first-lib/index.js
@@ -0,0 +1,11 @@
+import chalk from 'chalk'
+import fs from 'fs'
+
+function pegaArquivo(caminhoDoArquivo) {
+    const encoding = 'utf-8'
+    fs.readFile(caminhoDoArquivo, encoding, (_, texto) => {
+        console.log(chalk.green(texto))
+    })
+}
+
+pegaArquivo('./arquivos/texto.md')
