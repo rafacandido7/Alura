17dcd4f8a02cc53b6ff717f10add5ffabf091be6
Rafael Candido <rafacandido0708@gmail.com>
2022-11-06 19:55:59 -0300

    Starting tailwind config

--- ReactJs/organogram/tailwind.config.js
+++ ReactJs/organogram/tailwind.config.js
@@ -4,7 +4,33 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          blue: '#6278F7',
+          cian: '#95FFD4',
+          gray: '#F2F2F2',
+        },
+        bg: {
+          green:'#D9F7E9',
+          blue:'#E8F8FF',
+          lightGreen:'#F0F8E2',
+          red:'#FDE7E8',
+          pink:'#FAE9F5',
+          orange:'#FFF5D9',
+          darkOrange:'#FFEEDF',
+        },
+        contrast: {
+          green:'#57C278',
+          blue:'#82CFFA',
+          lightGreen:'#A6D157',
+          red:'#E06B69',
+          pink:'#DB6EBF',
+          orange:'#FFBA05',
+          darkOrange:'#FF8A29',
+        },
+      },
+    },
   },
   plugins: [],
 }
