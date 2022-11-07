bf422e393697c03a16b7e08ce54af48429df2183
Rafael Candido <rafacandido0708@gmail.com>
2022-11-07 12:21:14 -0300

    Adding Dropdown Component

--- ReactJs/organogram/src/components/Form/index.jsx
+++ ReactJs/organogram/src/components/Form/index.jsx
@@ -1,7 +1,18 @@
 import './styles.scss'
 import TextField from '../TextField'
+import Dropdown from '../Dropdown'
 
 const Form = (props) => {
+    const teamWorks = [
+        'Programação',
+        'Front-End',
+        'Data Science',
+        'Devops',
+        'UX e Design',
+        'Mobile',
+        'Inovação e Gestão'
+    ]
+
     return (
         <section className='form'>
             <form >
@@ -9,9 +20,7 @@ const Form = (props) => {
                 <TextField label='Nome' placeholder='Digite seu nome'/>
                 <TextField label='Cargo' placeholder='Digite seu cargo'/>
                 <TextField label='Imagem' placeholder='Digite o indereço da imagem'/>
+                <Dropdown label='Time' item={teamWorks} placeholder='Escolha seu time'></Dropdown>
             </form>
         </section>
     )
-}
-
-export default Form
