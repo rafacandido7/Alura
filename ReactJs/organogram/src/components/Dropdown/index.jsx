0c1c4263ca44cce47d038865e8f844b6438468ea
Rafael Candido <rafacandido0708@gmail.com>
2022-11-07 08:35:23 -0300

    Converting scss to tailwind on Dropdown component

--- ReactJs/organogram/src/components/Dropdown/index.jsx
+++ ReactJs/organogram/src/components/Dropdown/index.jsx
@@ -1,10 +1,8 @@
-import './styles.scss'
-
 const Dropdown = (props) => {
     return (
-        <div className='dropdown'>
-            <label>{props.label}</label>
-            <select>
+        <div className='text-base'>
+            <label className="block mb-2">{props.label}</label>
+            <select className=" bg-white shadow-[10px_10px_30px_0_rgba(0,0,0,0.06)] w-full border-none p-6 box-border">
                 {props.item.map(_item => {
                     return <option key={_item}>{_item}</option>
                 })}
